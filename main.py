66bc8c9e8f4217468f58587de615ec9cc24f52c3
Mitchell Breden <mail@mitchellbreden.nl>
2022-07-04 17:41:49 +0200

    edit the file

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import json
-
 import requests
 import logging
 from functools import partial
@@ -11,12 +9,12 @@ from shapely.geometry.polygon import Polygon
 proj_wgs84 = pyproj.Proj('+proj=longlat +datum=WGS84')
 
 
-def geodesic_point_buffer(lon, lat, km):
+def geodesic_point_buffer(lat, lon, km):
     # Azimuthal equidistant projection
     aeqd_proj = '+proj=aeqd +lat_0={lat} +lon_0={lon} +x_0=0 +y_0=0'
     project = partial(
         pyproj.transform,
-        pyproj.Proj(aeqd_proj.format(lon=lon, lat=lat)),
+        pyproj.Proj(aeqd_proj.format(lat=lat, lon=lon)),
         proj_wgs84)
     buf = Point(0, 0).buffer(km * 1000)  # distance in metres
 
@@ -24,33 +22,35 @@ def geodesic_point_buffer(lon, lat, km):
 
 
 def check(lons_lats_vect):
-    global point
+    # test purpose
+    # print(lons_lats_vect)
+    point = Point(5.9648594, 52.2086425)
+    # polygon = Polygon(lons_lats_vect)
+    # print(polygon.contains(point))
+
     polygon = Polygon(lons_lats_vect)  # create polygon
 
-    # url = "https://adsbx-flight-sim-traffic.p.rapidapi.com/api/aircraft/json/lat/52.208641/lon/5.96486/dist/25/"
-    url = "https://airlabs.co/api/v9/flights?api_key=46c11282-49fb-43e5-8741-554dd3a768ad";
-    # headers = {
-    #     "X-RapidAPI-Host": "adsbx-flight-sim-traffic.p.rapidapi.com",
-    #     "X-RapidAPI-Key": "343a3909c7mshdd02c4b847c51e0p1c1123jsnf35b8149da43"
-    # }
+    url = "https://adsbexchange-com1.p.rapidapi.com/v2/lat/52.2086425/lon/5.9648594/dist/3/"
+    # url = "https://airlabs.co/api/v9/flights?api_key=46c11282-49fb-43e5-8741-554dd3a768ad";
 
-    response = requests.request("GET", url)
-    r_dict = response.json()
+    headers = {
+        "X-RapidAPI-Key": "343a3909c7mshdd02c4b847c51e0p1c1123jsnf35b8149da43",
+        "X-RapidAPI-Host": "adsbexchange-com1.p.rapidapi.com"
+    }
 
-    for each in r_dict['response']:
-        point = Point(each['lng'], each['lat'])  # create point
+    response = requests.request("GET", url, headers=headers)
+    result = response.json()
 
+    for l in result['ac']:
+        point = Point(l['lon'], l['lat'])  # create point
         polygonCheck = point.within(polygon)  # check if a point is in the polygon
-        print(polygonCheck)
-        print(list(each))
-        print(len(r_dict['response']))
-
-    # point = Point(5.2009428, 52.0719623)
-    # polygonCheck = point.within(polygon)  # check if a point is in the polygon
-
+        print(l)
         if polygonCheck:
             payload = {
-                "test": polygonCheck
+                "inPolygon": polygonCheck,
+                "lat": l['lat'],
+                "lon": l['lon'],
+                "flight": l['flight']
             }
 
             requests.post('http://127.0.0.1:8000/api/flight-data', data=payload)
@@ -58,7 +58,7 @@ def check(lons_lats_vect):
             print("No flight data available")
 
 
-# Press the green button in the gutter to run the script.
+# Runs scripts
 if __name__ == '__main__':
-    b = geodesic_point_buffer(5.9648600, 52.2086400, 50)
+    b = geodesic_point_buffer(52.2086425, 5.9648594, 100.0)
     check(b)
