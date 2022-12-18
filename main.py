6fb25e2c1031bb1c94c2a60b71ee92b64f48d0ca
MitchBred <mail@mitchellbreden.nl>
2022-12-18 17:26:13 +0100

    add picamera2

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from shapely.ops import transform
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 import base64
+from picamera2 import Picamera2
 
+picam2 = Picamera2()
 proj_wgs84 = pyproj.Proj('+proj=longlat +datum=WGS84')
-
 
 def geodesic_point_buffer(lat, lon, km):
     # Azimuthal equidistant projection
@@ -24,7 +25,7 @@ def geodesic_point_buffer(lat, lon, km):
 def check(lons_lats_vect):
     polygon = Polygon(lons_lats_vect)  # create polygon
 
-    url = "https://adsbexchange-com1.p.rapidapi.com/v2/lat/52.2086425/lon/5.9648594/dist/10/"
+    url = "https://adsbexchange-com1.p.rapidapi.com/v2/lat/52.2086425/lon/5.9648594/dist/20/"
 
     headers = {
         "X-RapidAPI-Key": "343a3909c7mshdd02c4b847c51e0p1c1123jsnf35b8149da43",
@@ -37,21 +38,25 @@ def check(lons_lats_vect):
     for l in result['ac']:
         point = Point(l['lon'], l['lat'])  # create point
         polygonCheck = point.within(polygon)  # check if a point is in the polygon
+        polygonLower = str(polygonCheck).lower()
 
         if polygonCheck:
+
+            picam2.start_and_capture_file("images/flight.jpg") # capture
 
             with open("images/small.jpg", "rb") as img_file:
                 data_uri = base64.b64encode(img_file.read())
 
             try:
+                print('flight:', l['flight'])
                 payload = {
-                    "inPolygon": polygonCheck,
+                    "in_polygon": polygonLower,
                     "lat": l['lat'],
                     "lon": l['lon'],
                     "flight": l['flight'],
                     "image": data_uri
                 }
-                requests.post('https://flights.mitchellbreden.nl/api/flight-data', data=payload)
+                requests.post('https://huis.mitchellbreden.nl/api/flight-data', data=payload)
             except:
                 pass
         else:
@@ -60,5 +65,5 @@ def check(lons_lats_vect):
 
 # Runs scripts
 if __name__ == '__main__':
-    b = geodesic_point_buffer(52.2086425, 5.9648594, 50.0)
+    b = geodesic_point_buffer(52.2086425, 5.9648594, 150.0)
     check(b)
