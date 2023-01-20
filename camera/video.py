af31eff32f13d954da2eeba154c22ecc5f271bba
MitchBred <mail@mitchellbreden.nl>
2023-01-20 23:46:39 +0100

    try to fix photo

--- camera/video.py
+++ camera/video.py
@@ -6,7 +6,7 @@ picam2 = Picamera2()
 
 def record():
     # flightFormat = str(flightVideo).lower().strip()
-    picam2.start_and_record_video("videos/afr76up.mp4", duration=10)
+    picam2.start_and_record_video("afr76up.mp4", duration=10)
     # upload_video(flightFormat)
 
 
