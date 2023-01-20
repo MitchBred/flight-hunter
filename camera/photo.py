35752a59158026af77de35fb34305e007a7bdd81
MitchBred <mail@mitchellbreden.nl>
2023-01-20 23:20:44 +0100

    try to fix video

--- camera/photo.py
+++ camera/photo.py
@@ -10,5 +10,5 @@ picam2.start_preview(Preview.NULL)
 def capture(flightImage):
     picam2.start()
     time.sleep(2)
-    picam2.capture_file(flightImage)
+    picam2.capture_file("test.jpg")
     picam2.stop()
