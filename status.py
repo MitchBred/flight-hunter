686bb32f5b959caeae76c0814534cba51a0b8d55
MitchBred <mail@mitchellbreden.nl>
2023-12-11 21:32:09 +0100

    chore(weather-standby-mode): add clouds to payload

--- status.py
+++ status.py
@@ -4,13 +4,13 @@ import os
 
 def run(clouds):
     try:
-        clouds = f'The service is currently inactive.\nThe sky is {clouds}% covered with clouds.'
-        if clouds < os.getenv('WEATHER_CLOUD_PERCENTAGE'):
-            clouds = f"The service is active.\nThe sky is {clouds}% covered with clouds."
+        cloudText = f'The service is currently inactive.\nThe sky is {clouds}% covered with clouds.'
+        if clouds <= int(os.getenv('WEATHER_CLOUD_PERCENTAGE')):
+            cloudText = f"The service is active.\nThe sky is {clouds}% covered with clouds."
 
         payload = {
             "status": 201,
-            "clouds": {clouds}
+            "clouds": cloudText,
         }
         response = requests.post(os.getenv('RASPBERRY_PI_STATUS'), data=payload)
     except:
