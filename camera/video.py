05945817d270b7d302ac3b619b99a784384eb22d
MitchBred <mail@mitchellbreden.nl>
2025-03-08 20:52:02 +0100

    chore: code optimization and cleanup

--- camera/video.py
+++ camera/video.py
@@ -1,9 +1,6 @@
 import os
 import time
-
 import boto3
-from picamera2 import Picamera2
-
 
 def record(flightVideo):
     flight = str(flightVideo).lower().strip()
@@ -20,5 +17,13 @@ def upload_video(flight):
         's3',
         aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
         aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+        endpoint_url='https://367be3a2035528943240074d0096e0cd.r2.cloudflarestorage.com',
+        region_name='auto',
     )
-    client.upload_file(flight, 'mitch-flights', flight)
+
+    # Upload file to Cloudflare R2
+    bucket_name = 'fls-9e55c72d-5f79-467a-936f-bc2f4a6ea436'
+    client.upload_file(flight, bucket_name, flight)
+
+    # Delete the local file after uploading
+    os.remove(flight)
--- main.py
+++ main.py
@@ -1,103 +1,86 @@
 import os
-
-import pyproj
 import requests
 from dotenv import load_dotenv, find_dotenv
 from pyproj import CRS, Transformer
-from shapely.geometry import Point
-from shapely.geometry.polygon import Polygon
+from shapely.geometry import Point, Polygon
 from shapely.ops import transform
-import status
-import weather.api
 from calculations.nauticalmile import kilometer_to_nautical_mile
 from calculations.position_and_speed import distance_in_minutes
-
 # disable import for dev
 from camera import video
+import weather.api
+import status
 
-load_dotenv(find_dotenv())  # load env
+# Load environment variables
+load_dotenv(find_dotenv())
 
-script_dir = os.path.dirname(os.path.realpath(__file__))  # raspberry pi
-os.chdir(script_dir)
-
-proj_wgs84 = pyproj.Proj("+proj=longlat +datum=WGS84")
+# Set script directory
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 
 def geodesic_point_buffer(lat, lon, km):
-    # Azimuthal equidistant projection
-    aeqd_proj = CRS.from_proj4(
-        f"+proj=aeqd +lat_0={lat} +lon_0={lon} +x_0=0 +y_0=0")
+    """Generate a geodesic buffer around a point."""
+    aeqd_proj = CRS.from_proj4(f"+proj=aeqd +lat_0={lat} +lon_0={lon}")
     tfmr = Transformer.from_proj(aeqd_proj, aeqd_proj.geodetic_crs)
-    buf = Point(0, 0).buffer(km * 1000)  # distance in metres
+    buffer = Point(0, 0).buffer(km * 1000)  # Convert km to meters
+    return transform(tfmr.transform, buffer).exterior.coords[:]
 
-    return transform(tfmr.transform, buf).exterior.coords[:]
 
-
-def check(lons_lats_vect):
-    global flight_image, flight_video, item
-    polygon = Polygon(lons_lats_vect)  # create polygon
-    url = f"https://adsbexchange-com1.p.rapidapi.com/v2/lat/{os.getenv('LAT')}/lon/{os.getenv('LON')}/dist/{kilometer_to_nautical_mile()}/"
+def check(flight_area):
+    """Check for flights within the defined polygon."""
+    polygon = Polygon(flight_area)
+    url = (f"https://adsbexchange-com1.p.rapidapi.com/v2/lat/{os.getenv('LAT')}/"
+           f"lon/{os.getenv('LON')}/dist/{kilometer_to_nautical_mile()}/")
     headers = {
         "X-RapidAPI-Key": os.getenv("X_RAPID_API_KEY"),
         "X-RapidAPI-Host": os.getenv("X_RAPID_API_HOST"),
     }
-    response = requests.request("GET", url, headers=headers)
+    response = requests.get(url, headers=headers)
 
-    if response.status_code == 200:
-        flights = response.json()
-        if flights['ac'] is not None:
-            for item in flights['ac']:
-                # TODO: add if condition to ignore small flights
-                if 'flight' not in item:  # Check if 'flight' or 'ias' key exists in item
-                    print("Missing 'flight' key in item:", item)
-                    continue
-                if 'ias' not in item:
-                    print("Missing 'ias' key in item:", item)
-                    continue
+    if response.status_code != 200:
+        error_messages = {
+            503: "Server down | RAPID server is unavailable.",
+            429: "Server down | Too many requests.",
+        }
+        print(error_messages.get(response.status_code, "Server error | Invalid request or missing license."))
+        return
 
-                point = Point(item['lon'], item['lat'])  # create point
-                polygon_check = point.within(polygon)  # check if a point is in the polygon
-                polygon_lower = int(polygon_check)
+    flights = response.json().get('ac', [])
+    if not flights:
+        print(f'Flights | No flights in a {os.getenv("KM_RADIUS")} KM radius.')
+        return
 
-                if polygon_check:
-                    flight_image = "false"
-                    # flight_video = "videos/preview.mp4"
-                    flight_video = "videos/" + str(item["flight"]).lower().strip() + ".mp4"
+    for item in flights:
+        if 'flight' not in item or 'ias' not in item:
+            print("Incomplete flight data:", item)
+            continue
 
-                    try:
-                        payload = {
-                            "flight": item["flight"],
-                            "category": item['category'],
-                            "distance_in_minutes": distance_in_minutes(item['lat'], item['lon'], item.get('ias')),
-                            "in_polygon": polygon_lower,
-                            "lat": item["lat"],
-                            "lon": item["lon"],
-                            "image": flight_image,
-                            "video": flight_video
-                        }
-                        print(payload)
-                        requests.post(os.getenv('PROJECT_URL'), data=payload)
-                    except:
-                        pass
+        point = Point(item['lon'], item['lat'])
+        if point.within(polygon):
+            # flight_video = "videos/preview.mp4"
+            flight_video = "videos/" + str(item["flight"]).lower().strip() + ".mp4"
+            payload = {
+                "flight": item["flight"],
+                "category": item['category'],
+                "distance_in_minutes": distance_in_minutes(item['lat'], item['lon'], item['ias']),
+                "in_polygon": 1,
+                "lat": item["lat"],
+                "lon": item["lon"],
+                "image": "false",
+                "video": flight_video
+            }
+            print(payload)
+            requests.post(os.getenv('PROJECT_URL'), data=payload)
 
-                # check distance_in_minutes
-                if distance_in_minutes(item['lat'], item['lon'], item.get('ias')) < int(
-                        os.getenv("RECORD_VIDEO_LESS_THAN_DISTANCE")):
-                    print('Recording video')
-                    video.record(flight_video)
-        else:
-            print(f'Flights | no flights in kilometer area of {os.getenv("KM_RADIUS")} KM.', response.status_code)
-    elif response.status_code == 503:
-        print('Server down | the RAPID server is down.', response.status_code)
-    elif response.status_code == 429:
-        print('Server down | to many requests', response.status_code)
-    else:
-        print('Server down | wrong request or have no license.', response.status_code)
+        if distance_in_minutes(item['lat'], item['lon'], item['ias']) < int(
+                os.getenv("RECORD_VIDEO_LESS_THAN_DISTANCE")):
+            print('Recording video')
+            video.record(flight_video)
 
 
 if __name__ == "__main__":
-    clouds = weather.api.clouds()
-    status.run(clouds)
-    if clouds <= int(os.getenv('WEATHER_CLOUD_PERCENTAGE')):
-        buffer = geodesic_point_buffer(os.getenv("LAT"), os.getenv("LON"), int(os.getenv("KM_RADIUS")))
-        check(buffer)
+    # clouds = weather.api.clouds()
+    # status.run(clouds)
+    # if clouds <= int(os.getenv('WEATHER_CLOUD_PERCENTAGE')):
+    buffer = geodesic_point_buffer(os.getenv("LAT"), os.getenv("LON"), int(os.getenv("KM_RADIUS")))
+    check(buffer)
